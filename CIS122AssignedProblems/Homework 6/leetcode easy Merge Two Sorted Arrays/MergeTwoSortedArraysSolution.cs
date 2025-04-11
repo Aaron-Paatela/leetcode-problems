9c2ef5264924a8be54ffcf8ca3d9d0cdf04df47e
Aaron-Paatela <eh3375tx@go.minnstate.edu>
2025-04-11 16:02:51 -0500

    Add files via upload

---
+++ CIS122AssignedProblems/Homework 6/leetcode easy Merge Two Sorted Arrays/MergeTwoSortedArraysSolution.cs
@@ -0,0 +1,49 @@
+﻿// Written By Aaron Paatela
+// 4/2/2025
+
+// This is the Solution class for the easy leetcode problem 'Merge Sorted Arrays'
+// Please see Main Program for problem details
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_easy_Merge_Two_Sorted_Arrays
+{
+    public class MergeTwoSortedArraysSolution
+    {
+
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                nums1[m + i] = nums2[i];
+            }
+
+            bool swap = false;
+            int temp = 0;
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                swap = false;
+                for (int a = 0; a < nums1.Length - i - 1; a++)
+                {
+                    if (nums1[a] > nums1[a + 1])
+                    {
+                        temp = nums1[a];
+                        nums1[a] = nums1[a + 1];
+                        nums1[a + 1] = temp;
+                        swap = true;
+                    }
+
+                }
+                if (swap == false)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ CIS122AssignedProblems/Homework 6/leetcode easy Merge Two Sorted Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿// Written By Aaron Paatela
+// 4/2/2025
+
+// This is the Main Program to test the answer for the easy leetcode problem 'Merge Sorted Arrays'
+// leetcode problem here: https://leetcode.com/problems/merge-sorted-array/
+
+
+namespace leetcode_easy_Merge_Two_Sorted_Arrays
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers1 = new int[] { 2, 3, 2, 7, 4, 6, 0, 0, 0};
+            int[] numbers2 = new int[] {2, 3, 5};
+            int numbers1Length = 6;
+            int numbers2Length = 3;
+
+            MergeTwoSortedArraysSolution.Merge(numbers1, numbers1Length, numbers2, numbers2Length);
+
+            Console.Write($"[{string.Join(", ", numbers1)}]");
+        }
+    }
+}
