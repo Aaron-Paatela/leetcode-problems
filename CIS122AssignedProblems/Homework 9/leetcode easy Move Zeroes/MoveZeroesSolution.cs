ba4bac8cb7350967167006f40fb0f5e9dea73225
Aaron-Paatela <eh3375tx@go.minnstate.edu>
2025-04-28 10:49:57 -0500

    Add files via upload

---
+++ CIS122AssignedProblems/Homework 9/leetcode easy Move Zeroes/MoveZeroesSolution.cs
@@ -0,0 +1,40 @@
+﻿// Written By Aaron Paatela
+// 4/22/2025
+
+// This is my Solution class for the easy leetcode problem 'Move Zeroes'
+// Problem details here: https://leetcode.com/problems/move-zeroes/submissions/1620313724/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_easy_Move_Zeroes
+{
+    public class MoveZeroesSolution
+    {
+
+        public static void MoveZeroes(int[] nums)
+        {
+            if (nums == null || nums.Length <= 1)
+            {
+                return;
+            }
+
+            int current = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[current++] = nums[i];
+                }
+            }
+            for (; current < nums.Length; current++)
+            {
+                nums[current] = 0;
+            }
+        }
+    }
+}
---
+++ CIS122AssignedProblems/Homework 9/leetcode easy Move Zeroes/Program.cs
@@ -0,0 +1,17 @@
+﻿// Written By Aaron Paatela
+// 4/22/2025
+
+// This is the Main Program to test my solution class for the easy leetcode problem 'Move Zeroes'
+// Problem details here: https://leetcode.com/problems/move-zeroes/submissions/1620313724/
+
+
+namespace leetcode_easy_Move_Zeroes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
