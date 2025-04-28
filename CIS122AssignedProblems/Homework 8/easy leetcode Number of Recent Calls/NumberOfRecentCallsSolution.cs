2f54a2fcf48dc0d2bc8f5ce9f86a94253226d3f3
Aaron-Paatela <eh3375tx@go.minnstate.edu>
2025-04-28 10:18:52 -0500

    Add files via upload

---
+++ CIS122AssignedProblems/Homework 8/easy leetcode Number of Recent Calls/NumberOfRecentCallsSolution.cs
@@ -0,0 +1,37 @@
+﻿// Written By Aaron Paatela
+// 4/14/2025
+
+// This is the Main Program to test my solution for easy leetcode problem 'Number of Recent Calls
+// problem details here: https://leetcode.com/problems/number-of-recent-calls/description/
+
+// Main Program not really applicable for this problem, but I can't not have a Main Program
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easy_leetcode_Number_of_Recent_Calls
+{
+    internal class NumberOfRecentCallsSolution
+    {
+
+        private Queue<int> queue;
+        // I wasn't able to figure out how to get this to work on my own machine instead of at the leetcode site.
+
+        public RecentCounter()
+        {
+            queue = new Queue<int>();
+        }
+
+        public int Ping(int t)
+        {
+            queue.Enqueue(t);
+            while (t - queue.Peek() > 3000)
+                queue.Dequeue();
+            return queue.Count;
+        }
+    }
+}
---
+++ CIS122AssignedProblems/Homework 8/easy leetcode Number of Recent Calls/Program.cs
@@ -0,0 +1,19 @@
+﻿// Written By Aaron Paatela
+// 4/14/2025
+
+// This is the Main Program to test my solution for easy leetcode problem 'Number of Recent Calls
+// problem details here: https://leetcode.com/problems/number-of-recent-calls/description/
+
+// Main Program not really applicable for this problem, but I can't not have a Main Program
+
+
+namespace easy_leetcode_Number_of_Recent_Calls
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
